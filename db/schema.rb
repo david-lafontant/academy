2307849f2f14e75ca6e93249715f83dec271140f
David Lafontant <lafontant.david@gmail.com>
2024-04-18 19:13:02 -0400

    Generate schema.db

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_18_205617) do
 
   create_table "contents", force: :cascade do |t|
     t.string "title"
-    t.string "type"
+    t.string "file_type"
     t.string "file_path"
     t.bigint "lesson_id", null: false
     t.datetime "created_at", null: false

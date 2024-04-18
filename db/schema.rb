a53c21f0c27379050c57120a631f02cd237497e0
David Lafontant <lafontant.david@gmail.com>
2024-04-18 16:46:59 -0400

    Create lessons table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_18_203414) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_18_204508) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,6 +32,15 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_18_203414) do
     t.index ["user_id"], name: "index_courses_on_user_id"
   end
 
+  create_table "lessons", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.bigint "course_module_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_module_id"], name: "index_lessons_on_course_module_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.integer "role"
@@ -41,4 +50,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_18_203414) do
 
   add_foreign_key "course_modules", "courses"
   add_foreign_key "courses", "users"
+  add_foreign_key "lessons", "course_modules"
 end

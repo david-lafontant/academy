856971c644a9d1996d4c5a17ab2db4e4e6d84e2a
David Lafontant <lafontant.david@gmail.com>
2024-04-19 06:30:42 -0400

    List roles and set default role for user if not set

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable, :lockable, :trackable
+
   has_many :courses
   has_many :enrollements
   has_many :grades
+  enum role: %i[student instructor admin]
+
+  after_initialize :set_default_role, if: :new_record?
+
+  def set_default_role
+    self.role ||= :user
+  end
 end

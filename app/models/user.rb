3c5cd757ba62f49976aae4b9326f427efdf29623
David Lafontant <lafontant.david@gmail.com>
2024-04-19 21:37:22 -0400

    Set method to find if user is an admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  # attr_accessor :role
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -16,5 +17,9 @@ class User < ApplicationRecord
 
   def set_default_role
     self.role ||= :user
+  end
+
+  def admin?
+    self.role == 'admin'
   end
 end
